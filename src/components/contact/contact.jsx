7f683d653c5dcc5fb6ba1c51204516c2100f87e7
Brunogpr <85357465+Brunogpr@users.noreply.github.com>
2024-02-19 10:09:33 -0300

    Configuração de envio de e-mail

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        if(name === '' || email === '' || message === ''){
+            alert("Preencha todos os campos");
+            return;
+        };
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email
+        };
+
+        emailjs.send("service_b01u1ul", "template_y97tl9k", templateParams, "-282hJ5RP6iU-klOb")
+        .then((response) => {
+            alert("EMAIL ENVIADO")
+            console.log("EMAIL ENVIADO", response.status, response.text)
+            setName('')
+            setEmail('')
+            setSubject('')
+            setMessage('')
+        }, (err) => {
+            console.log("ERRO: ", err)
+        })
+    }
+
     return (
         <section className="contact container section" id='contact'>
             <h2 className="section__title">Entre em contato</h2>
@@ -12,32 +45,33 @@ const Contact = () => {
                     <p className="contact__details">Mande um e-mail e tire sua duvida. 👋</p>
                 </div>
 
-                <form action="" className="contact__form">
+                <form action="" className="contact__form" onSubmit={sendEmail}>
                     <div className="contact__form-group">
                         <div className="contact__form-div">
-                            <input type="text" className="contact__form-input" placeholder='Insira seu nome' />
+                            <input type="text" className="contact__form-input" placeholder='Insira seu nome' onChange={(e) => setName(e.target.value)} value={name} />
                         </div>
 
                         <div className="contact__form-div">
-                            <input type="email" className="contact__form-input" placeholder='Insira seu e-mail' />
+                            <input type="email" className="contact__form-input" placeholder='Insira seu e-mail' onChange={(e) => setEmail(e.target.value)} value={email} />
                         </div>
                     </div>
 
                     <div className="contact__form-div">
-                        <input type="text" className="contact__form-input" placeholder='Insira um assunto' />
+                        <input type="text" className="contact__form-input" placeholder='Insira um assunto' onChange={(e) => setSubject(e.target.value)} value={subject} />
                     </div>
 
                     <div className="contact__form-div contact__form-area">
-                        <textarea 
-                        name="" 
-                        id=""
-                        cols="30"
-                        rows="10" 
-                        className="contact__form-input"
-                        placeholder="Escreva sua mensagem"></textarea>
+                        <textarea
+                            name=""
+                            id=""
+                            cols="30"
+                            rows="10"
+                            className="contact__form-input"
+                            placeholder="Escreva sua mensagem"
+                            onChange={(e) => setMessage(e.target.value)} value={message}></textarea>
                     </div>
 
-                    <buttom className="btn">Enviar Mensagem</buttom>
+                    <button className="btn" type="submit" value="Enviar">Enviar Mensagem</button>
                 </form>
             </div>
         </section>
