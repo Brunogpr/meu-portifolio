a2013eacc14946de5257bfe9ab42c35e81935b9f
Brunogpr <85357465+Brunogpr@users.noreply.github.com>
2024-02-19 09:25:08 -0300

    Resposividade do portifolio

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./about.css";
-import Image from "../../assets/avatar-2.svg"
+import Image from "../../assets/eu01.png"
 import AboutBox from './AboutBox';
 
 
--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./sidebar.css";
 import Logo from "../../assets/logo.svg";
 
 const Sidebar = () => {
+    const[ toggle, showMenu ] = useState(false);
     return (
-        <aside className='aside'> 
+        <>
+        <aside className={toggle ? "aside show-menu" : "aside"}> 
             <a href="#home" className="nav__logo">
                 <img src={Logo} alt="" />
             </a>
@@ -61,6 +63,11 @@ const Sidebar = () => {
                 <span className='copyright'>&copy; 2024 -2024.</span>
             </div>
         </aside>
+        
+        <div className={toggle ? "nav__toggle nav__toggle-open" : "nav__toggle"} onClick={() => showMenu(!toggle)}>
+            <i className="icon-menu"></i>
+        </div>
+        </>
     )
 }
 
